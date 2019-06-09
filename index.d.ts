09e0390bf2dd4dfec3a617378d9612b8d546dde9
Lee JH <ljh86029926@gmail.com>
2019-06-09 21:02:13 +0900

    Add type definition (#24)
    * Add type definition
    
    * Add another html attributes

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for react-tiny-fab 2.0
+// Project: https://github.com/dericgw/react-tiny-fab, https://dericgw.github.io/react-tiny-fab/
+// Definitions by:  Ji-Hoon Lee <https://github.com/NoMoreViolence>
+// TypeScript Version: 3.4.5
+
+import { ReactElement, DOMAttributes, CSSProperties, HTMLAttributes } from 'react';
+
+interface ActionProps {
+  text?: string;
+  children?: ReactElement;
+}
+interface FabProps {
+  mainButtonStyles?: CSSProperties;
+  position?: { bottom?: number; top?: number; right?: number; left?: number };
+  icon: ReactElement;
+  event?: 'hover' | 'click';
+  children?: ReactElement[] | ReactElement;
+}
+
+type Action = (props: ActionProps & DOMAttributes<HTMLElement> & HTMLAttributes<HTMLElement>) => ReactElement;
+type Fab = (props: FabProps) => ReactElement;
+
+declare const Action: Action;
+declare const Fab: Fab;
