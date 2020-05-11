70f2001fcf010f1ea2d046095dfb209e39d20d1f
Dovy Paukstys <me@dovy.io>
2020-05-11 12:10:56 -0400

    Allow `text` to show even if onClick isn't used.
    This should work both ways.  ;)

--- src/index.jsx
+++ src/index.jsx
@@ -95,7 +95,7 @@ const Fab = ({
         >
           {icon}
         </MB>
-        {onClick && text && (
+        {text && (
           <span
             className={`${'right' in position ? 'right' : ''} ${
               alwaysShowTitle ? 'always-show' : ''
